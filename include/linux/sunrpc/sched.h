70352eed4100ab63a5a61f834dc632884a6db268
Rahul Iyer <iyer@netapp.com>
2007-08-21 15:27:56 -0700

    nfs41: sunrpc: client side rpc changes
    [was: The new reworked slot table. Async ops don't work]
    
    From: root <root@sager-fedora.(none)>
    
    Signed-off-by: Rahul Iyer <iyer@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    nfs41: Removed check for call/reply flag in call_verify
    
    This will be checked in xs_tcp_read_request
    
    Signed-off-by: root <root@sager-fedora.(none)>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- include/linux/sunrpc/sched.h
+++ include/linux/sunrpc/sched.h
@@ -113,6 +113,9 @@ typedef void			(*rpc_action)(struct rpc_task *);
 
 struct rpc_call_ops {
 	void (*rpc_call_prepare)(struct rpc_task *, void *);
+#if defined(CONFIG_NFS_V4_1)
+	void (*rpc_call_validate_args)(struct rpc_task *, void *);
+#endif /* CONFIG_NFS_V4_1 */
 	void (*rpc_call_done)(struct rpc_task *, void *);
 	void (*rpc_release)(void *);
 };
