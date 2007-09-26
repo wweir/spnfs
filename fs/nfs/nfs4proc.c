146a81d2f641d6c1b96812e510357ddc419f7956
Rahul Iyer <iyer@netapp.com>
2007-09-25 17:28:12 -0700

    pnfs: Made nfslayoutdriver a module
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -4906,6 +4906,7 @@ int nfs4_proc_destroy_session(struct nfs_server *sp)
 	dprintk("<-- nfs4_proc_destroy_session\n");
 	return status;
 }
+EXPORT_SYMBOL(nfs4_proc_destroy_session);
 
 int nfs4_proc_sequence(struct nfs_client *clp, struct rpc_cred *cred)
 {
@@ -5358,6 +5359,7 @@ const struct nfs_rpc_ops *nfsv4_minorversion_clientops[] = {
 	&pnfs_v41_clientops,
 #endif
 };
+
 
 /*
  * Local variables:
