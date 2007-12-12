1c09f57afa85bb416a2832491f1da10350cf8c23
Benny Halevy <bhalevy@panasas.com>
2007-12-12 19:01:50 +0200

    pnfsd: Integrated remaining callback patches
    split off of 0762d5c4f3f1eb8c9b5d3f4c0bf8672a90990eaf
    Integrated remaining NFSv4.1 callback channel and cb sequence patches.
    
    Signed-off-by: root <root@sager-fedora.(none)>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- include/linux/nfsd/nfsd4_pnfs.h
+++ include/linux/nfsd/nfsd4_pnfs.h
@@ -121,6 +121,7 @@ struct nfsd4_pnfs_cb_layout {
 	u32                     cbl_recall_type;	/* request */
 	struct nfsd4_layout_seg cbl_seg;		/* request */
 	u32                     cbl_layoutchanged;	/* request */
+	struct nfs4_fsid        cbl_fsid;
 };
 
 #endif /* CONFIG_PNFSD */
