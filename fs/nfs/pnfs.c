e6cd5193116eac2289acac77663cbd2b914cebee
Andy Adamson <andros@umich.edu>
2007-12-13 15:51:44 -0500

    pnfs: remove PNFS_USE_DS flag
    In 2.6.18.3, there are several read I/O paths through the nfs client code.
    The PNFS_USE_DS flag was set to have an easy way to check if
    I/O was to be performed to the storage device.
    
    In 2.6.24, there is only one read I/O path through the client code. The
    interface to the nfs page cache has been re-written. It is now shared between
    the read and write code paths, and moved entirely to fs/nfs/pagelist.c.
    
    There is no need for the PNFS_USE_DS flag.
    
    Signed-off-by: Andy Adamson<andros@umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -993,7 +993,6 @@ int _pnfs_try_to_read_data(struct nfs_read_data *data,
 	} else {
 		dprintk("%s: Utilizing pNFS I/O\n", __func__);
 		data->call_ops = call_ops;
-		data->pnfsflags |= PNFS_USE_DS;
 		return pnfs_readpages(data);
 	}
 }
@@ -1013,7 +1012,6 @@ int pnfs_try_to_write_data(struct nfs_write_data *data,
 	} else {
 		dprintk("%s: Utilizing pNFS I/O\n", __func__);
 		data->call_ops = call_ops;
-		data->pnfsflags |= PNFS_USE_DS;
 		data->how = how;
 		return pnfs_writepages(data, how);
 	}
@@ -1320,7 +1318,6 @@ pnfs_wsize(struct inode *inode, unsigned int count, struct nfs_write_data *wdata
 	if (count >= 0 && below_threshold(inode, count, 1))
 		return NFS_SERVER(inode)->wsize;
 
-	wdata->pnfsflags |= PNFS_USE_DS;
 	return NFS_SERVER(inode)->ds_wsize;
 }
 
