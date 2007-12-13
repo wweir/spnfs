54579997a7bef10892461d100b7cd208a4863aad
Andy Adamson <andros@umich.edu>
2007-12-13 15:51:49 -0500

    pnfs: use boundary in coalese page cache request
    Add a new layout policy operation to test two struct nfs_page requests for
    coalescing.
    
    Add test function and boundary int to the nfs_pageio_descriptor structure.
    
    Signed-off-by: Andy Adamson<andros@umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- include/linux/nfs_xdr.h
+++ include/linux/nfs_xdr.h
@@ -1169,7 +1169,6 @@ struct nfs_rpc_ops {
 	int	(*wsize) (struct inode *, unsigned int, struct nfs_write_data *);
 	int	(*rpages) (struct inode *);
 	int	(*wpages) (struct inode *);
-	u32	(*boundary) (struct inode *);
 	int	(*pnfs_layoutget)(struct nfs4_pnfs_layoutget *layout);
 	void	(*pnfs_layoutcommit_setup)(struct pnfs_layoutcommit_data *);
 	int	(*pnfs_layoutcommit)  (struct pnfs_layoutcommit_data *);
