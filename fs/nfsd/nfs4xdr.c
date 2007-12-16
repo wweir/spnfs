dd9b98f2294eeedcfc3d43e1399d57299e02ed2b
Benny Halevy <bhalevy@panasas.com>
2007-12-16 11:43:49 +0200

    nfsd41: pnfs ops return nfserr_notsupp
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfsd/nfs4xdr.c
+++ fs/nfsd/nfs4xdr.c
@@ -1417,6 +1417,13 @@ nfsd4_decode_compound(struct nfsd4_compoundargs *argp)
 		case OP_DESTROY_SESSION:
 			op->status = nfsd4_decode_destroy_session(argp, &op->u.destroy_session);
 			break;
+		case OP_GETDEVICELIST:
+		case OP_GETDEVICEINFO:
+		case OP_LAYOUTGET:
+		case OP_LAYOUTCOMMIT:
+		case OP_LAYOUTRETURN:
+			op->status = nfserr_notsupp;
+			break;
 #endif /* CONFIG_NFSD_V4_1 */
 		default:
 			op->opnum = OP_ILLEGAL;
