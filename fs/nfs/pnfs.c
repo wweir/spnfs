d19d10f609b54349167e8b4d7afae77b48237e58
Benny Halevy <bhalevy@panasas.com>
2007-12-30 15:39:11 +0200

    pnfs: initialize args.new_layout in pnfs_layoutcommit_setup
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -1344,6 +1344,7 @@ pnfs_layoutcommit_setup(struct pnfs_layoutcommit_data *data, int sync)
 	 * the actual size of an updated layout.
 	 */
 	data->args.new_layout_size = 0;
+	data->args.new_layout = NULL;
 
 	/* TODO: Need to determine the correct values */
 	data->args.time_modify_changed = 0;
