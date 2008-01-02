68cba09f0dd23f9a97f40cc2117dc08d9b7c202b
Benny Halevy <bhalevy@panasas.com>
2008-01-02 16:49:44 +0200

    pnfs: pass nr_pages to pnfs_set_ds_rsize
    use number of pages rather than traversing the pages array
    to determine the count for pnfs_update_layout and I/O threshold.
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/read.c
+++ fs/nfs/read.c
@@ -640,7 +640,8 @@ int nfs_readpages(struct file *filp, struct address_space *mapping,
 	} else
 		desc.ctx = get_nfs_open_context(nfs_file_open_context(filp));
 #ifdef CONFIG_PNFS
-	pnfs_set_ds_rsize(inode, desc.ctx, pages, filp->f_pos, &rsize, &pgio);
+	pnfs_set_ds_rsize(inode, desc.ctx, pages, nr_pages, filp->f_pos,
+			  &rsize, &pgio);
 #endif /* CONFIG_PNFS */
 	if (rsize < PAGE_CACHE_SIZE)
 		nfs_pageio_init(&pgio, inode, nfs_pagein_multi, rsize, 0);
