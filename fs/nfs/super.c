45b4c101da280c604b72ce29c761663d64dd8fa8
Benny Halevy <bhalevy@panasas.com>
2008-01-02 17:06:55 +0200

    pnfs: initialize ds_[rw]size only after the layout driver has registered
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/super.c
+++ fs/nfs/super.c
@@ -1705,6 +1705,7 @@ int nfs4_init_pnfs(struct super_block *sb, struct nfs_server *server,
 	switch (clp->cl_minorversion) {
 	case 1:
 		set_pnfs_layoutdriver(sb, fh, server->pnfs_fs_ltype);
+		pnfs_set_ds_iosize(server);
 		break;
 	case 0:
 		break;
