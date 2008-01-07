c4f01cd3fbdd0dba6456792682b8e025643d47e6
Andy Adamson <andros@umich.edu>
2008-01-07 15:47:30 -0500

    pnfs: client get layout in commit path
    Check for a cached layout prior to flushing writes.
    
    Signed-off-by: Andy Adamson<andros@umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.h
+++ fs/nfs/pnfs.h
@@ -52,6 +52,7 @@ int pnfs_try_to_commit(struct inode *, struct nfs_write_data *, struct list_head
 void pnfs_commit_done_norpc(struct rpc_task *, void *);
 void pnfs_pageio_init_read(struct nfs_pageio_descriptor *, struct inode *, struct nfs_open_context *, struct list_head *, size_t *);
 void pnfs_pageio_init_write(struct nfs_pageio_descriptor *, struct inode *);
+void pnfs_update_layout_commit(struct inode *, struct list_head *, pgoff_t, unsigned int);
 
 #endif /* CONFIG_PNFS */
 
