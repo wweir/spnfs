96316f3772c065c4b3c3f63fb2a35578bab8b541
Andy Adamson <andros@umich.edu>
2008-01-07 15:47:28 -0500

    pnfs client layoutcommit
    Implement the layoutcommit operation
    
    Signed-off by: Andy Adamson<andros@umich.edu>

--- fs/nfs/inode.c
+++ fs/nfs/inode.c
@@ -1005,6 +1005,16 @@ static int nfs_update_inode(struct inode *inode, struct nfs_fattr *fattr)
 			&& !nfs_fsid_equal(&server->fsid, &fattr->fsid))
 		server->fsid = fattr->fsid;
 
+#ifdef CONFIG_PNFS
+	/*
+	* file needs layout commit, server attributes may be stale
+	*/
+	if (nfsi->layoutcommit_ctx && nfsi->change_attr >= fattr->change_attr) {
+		dprintk("NFS: %s: layoutcommit is needed for file %s/%ld\n",
+				__FUNCTION__, inode->i_sb->s_id, inode->i_ino);
+		return 0;
+	}
+#endif /* CONFIG_PNFS */
 	/*
 	 * Update the read time so we don't revalidate too often.
 	 */
@@ -1041,7 +1051,13 @@ static int nfs_update_inode(struct inode *inode, struct nfs_fattr *fattr)
 	if (new_isize != cur_isize) {
 		/* Do we perhaps have any outstanding writes, or has
 		 * the file grown beyond our last write? */
+#ifdef CONFIG_PNFS
+		/* XXX Andros: is the check for layoutcommit_ctx needed? */
+		if (nfsi->npages == 0 || new_isize > cur_isize ||
+		    !nfsi->layoutcommit_ctx) {
+#else
 		if (nfsi->npages == 0 || new_isize > cur_isize) {
+#endif /* CONFIG_PNFS */
 			inode->i_size = new_isize;
 			invalid |= NFS_INO_INVALID_ATTR|NFS_INO_INVALID_DATA;
 		}
