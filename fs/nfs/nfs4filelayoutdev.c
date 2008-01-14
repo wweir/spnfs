8d4067b7f2abcfc2bce2437a2338dd249b30d538
Dean Hildebrand <seattleplus@gmail.com>
2008-01-14 14:41:48 -0800

    pnfs: Align name and increase size of maxcount
    It is confusing that maxcount in the code refers to number
    of devices while in the spec it refers to number of bytes.
    
    Use number of bytes with getdevinfo and getdevlist, not max
    number of devices.
    
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>

--- fs/nfs/nfs4filelayoutdev.c
+++ fs/nfs/nfs4filelayoutdev.c
@@ -595,7 +595,7 @@ decode_and_add_devicelist(struct filelayout_mount_type *mt,
 	dprintk("%s invoked.  num_devs=%d\n", __func__, devlist->num_devs);
 
 	for (i = 0, cnt = 0;
-	     i < devlist->num_devs && cnt < NFS4_PNFS_DEV_MAXCOUNT;
+	     i < devlist->num_devs && cnt < NFS4_PNFS_DEV_MAXNUM;
 	     i++) {
 		if (!decode_and_add_device(mt, &devlist->devs[cnt])) {
 			dprintk("%s: error count=%d\n", __func__, cnt);
