7aad0cc47fac1925e3e78454ce855687ee0acb32
Benny Halevy <bhalevy@panasas.com>
2008-01-20 14:37:54 +0200

    nfs41: define nfs41_{set,test}_session_expired in header file
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- include/linux/nfs41_session_recovery.h
+++ include/linux/nfs41_session_recovery.h
@@ -11,13 +11,32 @@
 
 #if defined(CONFIG_NFS_V4_1)
 
-int nfs41_set_session_expired(struct nfs4_session *);
+/*
+ * Session state bits
+ */
+enum nfs41_session_state {
+	NFS41_SESSION_EXPIRED = 0,
+	NFS41_SESSION_RECOVER,
+};
+
+/*
+ * Set the session state expired
+ */
+static inline int nfs41_set_session_expired(struct nfs4_session *session)
+{
+	return test_and_set_bit(NFS41_SESSION_EXPIRED, &session->session_state);
+}
+
+static inline int nfs41_test_session_expired(struct nfs4_session *session)
+{
+	return test_bit(NFS41_SESSION_EXPIRED, &session->session_state);
+}
+
 int nfs41_set_session_valid(struct nfs4_session *);
 int nfs41_recover_session(struct nfs_client *, struct nfs4_session *);
 int nfs41_recover_session_sync(struct rpc_clnt *, struct nfs_client *, struct nfs4_session *);
 int nfs41_recover_session_async(struct rpc_task *, struct nfs_server *);
 int nfs41_recover_expired_session(struct rpc_task *, struct nfs_client *, struct nfs4_session *);
-int nfs41_test_session_expired(struct nfs4_session *);
 
 #endif	/* CONFIG_NFS_V4_1 */
 #endif	/* __NFS41_SESSION_RECOVERY_H__ */
