cd0e1c8899dc0958f0242fc53746e745b34a2d1e
Ricardo Labiaga <ricardo.labiaga@netapp.com>
2008-01-31 03:57:03 -0800

    [FIXME] nfs41: Backchannel callback service helper routines
    Executes the backchannel task on the RPC state machine using
    the existing open connection previously established by the client.
    
    Signed-off-by: Ricardo Labiaga <ricardo.labiaga@netapp.com>
    
    nfs41: Add bc_svc.o to sunrpc Makefile.
    
    Signed-off-by: Ricardo Labiaga <ricardo.labiaga@netapp.com>
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    [FIXME]: pull "nfs41: Add backchannel processing support to RPC state machine"
    before this one to resolve depndencies on bc_prealloc and on rpc_run_bc_task

--- net/sunrpc/xprtsock.c
+++ net/sunrpc/xprtsock.c
@@ -2199,6 +2199,7 @@ static struct rpc_xprt_ops xs_tcp_ops = {
 	.buf_free		= rpc_free,
 	.send_request		= xs_tcp_send_request,
 	.set_retrans_timeout	= xprt_set_retrans_timeout_def,
+	.release_request	= bc_release_request,
 	.close			= xs_tcp_shutdown,
 	.destroy		= xs_destroy,
 	.print_stats		= xs_tcp_print_stats,
