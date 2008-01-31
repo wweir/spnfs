b9e9dd4326dfb4818346ea18c9ced3c18f0bf91f
Rahul Iyer <iyer@netapp.com>
2008-01-31 03:57:04 -0800

    nfs41: Get the rpc_xprt * from the rpc_rqst instead of the rpc_clnt.
    Obtain the rpc_xprt from the rpc_rqst so that calls and callback replies
    can both use the same code path.  A client needs the rpc_xprt in order
    to reply to a callback.
    
    Signed-off-by: Rahul Iyer <iyer@netapp.com>
    Signed-off-by: Ricardo Labiaga <ricardo.labiaga@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- net/sunrpc/xprt.c
+++ net/sunrpc/xprt.c
@@ -165,8 +165,8 @@ EXPORT_SYMBOL_GPL(xprt_unregister_transport);
  */
 int xprt_reserve_xprt(struct rpc_task *task)
 {
-	struct rpc_xprt	*xprt = task->tk_xprt;
 	struct rpc_rqst *req = task->tk_rqstp;
+	struct rpc_xprt	*xprt = req->rq_xprt;
 
 	if (test_and_set_bit(XPRT_LOCKED, &xprt->state)) {
 		if (task == xprt->snd_task)
@@ -816,7 +816,7 @@ out_unlock:
 
 void xprt_end_transmit(struct rpc_task *task)
 {
-	xprt_release_write(task->tk_xprt, task);
+	xprt_release_write(task->tk_rqstp->rq_xprt, task);
 }
 
 /**
