0f23e109171218d084ceaf52cce8549e9089d61a
Andy Adamson <andros@umich.edu>
2008-03-14 12:23:46 -0400

    pnfs: pnfs refactor return layout
    Allocate nfs4_pnfs_layoutreturn for async rpc. Remove pnfs_return_layout_rpc.
    Free the calldata in the rpc_release function.
    
    Signed-off-by: Andy Adamson<andros@umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -5225,24 +5225,25 @@ static int
 nfs4_pnfs_layoutreturn_validate(struct rpc_task *task, void *calldata)
 {
 	struct nfs4_pnfs_layoutreturn *lrp = calldata;
-	struct inode *ino = lrp->args->inode;
+	struct inode *ino = lrp->args.inode;
 	struct nfs_server *server = NFS_SERVER(ino);
 
 	dprintk("--> %s\n", __func__);
 	return nfs4_setup_sequence(server->nfs_client, server->session,
-				   &lrp->args->seq_args,
-				   &lrp->res->seq_res,
+				   &lrp->args.seq_args,
+				   &lrp->res.seq_res,
 				   0, task);
 }
 
 static void nfs4_pnfs_layoutreturn_done(struct rpc_task *task, void *calldata)
 {
 	struct nfs4_pnfs_layoutreturn *lrp = calldata;
-	struct inode *ino = lrp->args->inode; struct nfs_server *server = NFS_SERVER(ino);
+	struct inode *ino = lrp->args.inode;
+	struct nfs_server *server = NFS_SERVER(ino);
 
 	dprintk("--> %s\n", __func__);
 
-	nfs4_sequence_done(server, &lrp->res->seq_res, task->tk_status);
+	nfs4_sequence_done(server, &lrp->res.seq_res, task->tk_status);
 	if (RPC_ASSASSINATED(task))
 		return;
 
@@ -5252,7 +5253,7 @@ static void nfs4_pnfs_layoutreturn_done(struct rpc_task *task, void *calldata)
 static void nfs4_pnfs_layoutreturn_release(void *calldata)
 {
 	struct nfs4_pnfs_layoutreturn *lrp = calldata;
-	struct nfs_inode *nfsi = NFS_I(lrp->args->inode);
+	struct nfs_inode *nfsi = NFS_I(lrp->args.inode);
 	struct pnfs_layout_type *lo;
 
 	lo = nfsi->current_layout;
@@ -5260,6 +5261,7 @@ static void nfs4_pnfs_layoutreturn_release(void *calldata)
 
 	dprintk("--> %s\n", __func__);
 	pnfs_layout_release(lo);
+	kfree(calldata);
 	dprintk("<-- %s\n", __func__);
 }
 
@@ -5271,13 +5273,13 @@ static const struct rpc_call_ops nfs4_pnfs_layoutreturn_call_ops = {
 
 static int pnfs4_proc_layoutreturn(struct nfs4_pnfs_layoutreturn *lrp)
 {
-	struct inode *ino = lrp->args->inode;
+	struct inode *ino = lrp->args.inode;
 	struct nfs_server *server = NFS_SERVER(ino);
 	struct rpc_task *task;
 	struct rpc_message msg = {
 		.rpc_proc = &nfs4_procedures[NFSPROC4_CLNT_PNFS_LAYOUTRETURN],
-		.rpc_argp = lrp->args,
-		.rpc_resp = lrp->res,
+		.rpc_argp = &lrp->args,
+		.rpc_resp = &lrp->res,
 	};
 	struct rpc_task_setup task_setup_data = {
 		.rpc_client = server->client,
