76fc5c1847af3c70c1b4d34ebf0e2a3835540845
Andy Adamson <andros@umich.edu>
2008-03-14 12:23:45 -0400

    pnfs: add recall type to pnfs_return_layout
    Simplify pnfs_recall_layout.
    Preparation for refactoring of pnfs_return_layout.
    
    Signed-off-by: Andy Adamson<andros@umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/nfs4state.c
+++ fs/nfs/nfs4state.c
@@ -506,7 +506,7 @@ static void __nfs4_close(struct path *path, struct nfs4_state *state, mode_t mod
 		range.iomode = nfsi->current_layout->roc_iomode;
 		range.offset = 0;
 		range.length = NFS4_LENGTH_EOF;
-		pnfs_return_layout(state->inode, &range);
+		pnfs_return_layout(state->inode, &range, RECALL_FILE);
 	}
 #endif /* CONFIG_PNFS */
 		nfs4_do_close(path, state, wait);
