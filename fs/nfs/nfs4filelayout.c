243ea085e5c8f3e6a1b6f616d04af4d79ace629c
Fred Isaman <iisaman@citi.umich.edu>
2008-03-14 10:18:47 -0400

    pnfsfile: Remove duplicated code in filelayout_read_pagelist
    Some offset setting code was duplicated via copy and paste, and then
    called unecessarily in many cases.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/nfs4filelayout.c
+++ fs/nfs/nfs4filelayout.c
@@ -305,13 +305,6 @@ static int filelayout_read_pagelist(
 	}
 
 	/* Perform an asynchronous read */
-	/* Now get the file offset on the dserver
-	 * Set the write offset to this offset, and
-	 * save the original offset in orig_offset
-	 */
-	data->args.offset = filelayout_get_dserver_offset(offset, flseg);
-	data->orig_offset = offset;
-
 	BUG_ON(data->pnfsflags & PNFS_ISSYNC);
 	nfs_initiate_read(data, data->pnfs_client, &filelayout_read_call_ops);
 
