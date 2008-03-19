391e967442f0bcc01c628e921e0e7c63f8d4dd8f
Benny Halevy <bhalevy@panasas.com>
2008-03-19 18:43:54 +0200

    nfs41: create_session operation
    Signed-off-by: Ricardo Labiaga <labiaga@labiaga-ll.(none)>
    Signed-off-by: Andy Adamson<andros@umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    nfs41: Fix client to allow more than one NFSv4.1 sessions
    
    The client currently uses the sequence ID returned by the result of
    the EXCHANGE_ID operation on every CREATE_SESSION request.  This is
    incorrect.  It should use the sequence ID returned by EXCHANGE_ID on
    the first CREATE_SESSION request.  If successful, a subsequent
    requests should use sequence ID + 1.  If successful, and a third
    CREATE_SESSION is requested, it should use sequence ID + 2,
    and so on.
    
    Signed-off-by: Ricardo Labiaga <ricardo.labiaga@netapp.com>
    
    nfs41: Fix create_session client xdr max alloc
    
    Signed-off-by: Dean Hildebrand <dhildeb@us.ibm.com>
    
    nfs41: Return negative error on CREATE_SESSION failure
    
    Signed-off-by: Ricardo Labiaga <Ricardo.Labiaga@netapp.com>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    nfs41: get rid of nr_sequence_quads
    
    This isn't used anymore.
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    nfs41: fix debug print in nfs4_proc_create_session
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- include/linux/nfs_xdr.h
+++ include/linux/nfs_xdr.h
@@ -1002,6 +1002,20 @@ struct nfs41_exchange_id_res {
 	struct server_scope		server_scope;
 	struct nfs_impl_id4		impl_id;
 };
+
+struct nfs41_create_session_args {
+	struct nfs_client	       *client;
+	struct nfs4_session	       *session;
+	uint32_t			flags;
+	uint32_t			cb_program;
+	struct nfs4_channel_attrs	fc_attrs;	/* Fore Channel */
+	struct nfs4_channel_attrs	bc_attrs;	/* Back Channel */
+};
+
+struct nfs41_create_session_res {
+	struct nfs_client	       *client;
+	struct nfs4_session	       *session;
+};
 #endif /* CONFIG_NFS_V4_1 */
 
 struct nfs_page;
