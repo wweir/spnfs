d01c7bfe314ff27bff2b3cd48a25f68d6b1be5b4
Fred Isaman <iisaman@citi.umich.edu>
2008-03-25 16:24:16 -0400

    [SQUASHME] pnfs: fix compile problems if CONFIG_PNFS turned off - callback_proc.c
    Mostly, #include of header files should not encosed in #ifdef,
    since *.h files themselves have the appropriate #ifdefs, and they may want
    to define no-op versions of some functions.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/callback_proc.c
+++ fs/nfs/callback_proc.c
@@ -16,9 +16,9 @@
 #include <linux/kthread.h>
 #include <linux/module.h>
 #include <linux/writeback.h>
+#endif
 
 #include "pnfs.h"
-#endif
 
 #ifdef NFS_DEBUG
 #define NFSDBG_FACILITY NFSDBG_CALLBACK
