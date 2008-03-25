7f47eceb3779025ec624c5de3e171d6f853853cf
Fred <iisaman@citi.umich.edu>
2008-03-25 16:24:23 -0400

    pnfs: pnfs_write_begin
    Add hooks in the nfs_write_begin path, giving a driver the potential
    to read in page data around the data that is about to be copied to the page.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -807,6 +807,28 @@ pnfs_has_layout(struct pnfs_layout_type *lo,
 	return ret;
 }
 
+static struct pnfs_layout_segment *
+pnfs_find_get_lseg(struct inode *inode,
+		   loff_t pos,
+		   size_t count,
+		   enum pnfs_iomode iomode)
+{
+	struct nfs_inode *nfsi = NFS_I(inode);
+	struct pnfs_layout_segment *lseg;
+	struct pnfs_layout_type *lo;
+	struct nfs4_pnfs_layout_segment range;
+
+	lo = get_lock_current_layout(nfsi);
+	if (!lo)
+		return NULL;
+	range.iomode = iomode;
+	range.offset = pos;
+	range.length = count;
+	lseg = pnfs_has_layout(lo, &range, 1);
+	put_unlock_current_layout(nfsi, lo);
+	return lseg;
+}
+
 /* Update the file's layout for the given range and iomode.
  * Layout is retreived from the server if needed.
  * If lsegpp is given, the appropriate layout segment is referenced and
@@ -1544,6 +1566,52 @@ int _pnfs_try_to_read_data(struct nfs_read_data *data,
 	}
 }
 
+/*
+ * This gives the layout driver an opportunity to read in page "around"
+ * the data to be written.  It returns 0 on success, otherwise an error code
+ * which will either be passed up to user, or ignored if
+ * some previous part of write succeeded.
+ * Note the range [pos, pos+len-1] is entirely within the page.
+ */
+/* flags = AOP_FLAG_UNINTERRUPTIBLE or 0 - ??? need this ??? */
+int _pnfs_write_begin(struct inode *inode, struct nfs_server *nfss,
+		      struct page *page, loff_t pos, unsigned len,
+		      unsigned flags, struct pnfs_fsdata **fsdata)
+{
+	struct pnfs_layout_segment *lseg;
+	int status = 0;
+
+	lseg = pnfs_find_get_lseg(inode, pos, len, IOMODE_RW);
+	if (lseg) {
+		status = nfss->pnfs_curr_ld->ld_io_ops->write_begin(lseg, page,
+								    pos, len,
+								    fsdata);
+		put_lseg(lseg);
+		if (*fsdata && !(*fsdata)->ld_io_ops)
+			(*fsdata)->ld_io_ops = nfss->pnfs_curr_ld->ld_io_ops;
+	}
+	return status;
+}
+
+/* Given an nfs request, determine if it should be flushed before proceeding.
+ * It should default to returning False, returning True only if there is a
+ * specific reason to flush.
+ */
+int _pnfs_do_flush(struct inode *inode, struct nfs_server *nfss,
+		   struct nfs_page *req, struct pnfs_fsdata *fsdata)
+{
+	struct pnfs_layout_segment *lseg;
+	loff_t pos = (req->wb_index << PAGE_CACHE_SHIFT) + req->wb_offset;
+	int status = 0;
+
+	lseg = pnfs_find_get_lseg(inode, pos, req->wb_bytes, IOMODE_RW);
+	/* Note that lseg==NULL may be useful info for do_flush */
+	status = nfss->pnfs_curr_ld->ld_policy_ops->do_flush(lseg, req,
+							     fsdata);
+	put_lseg(lseg);
+	return status;
+}
+
 int _pnfs_try_to_write_data(struct nfs_write_data *data,
 			    const struct rpc_call_ops *call_ops, int how)
 {
@@ -1862,6 +1930,14 @@ void pnfs_free_request_data(struct nfs_page *req)
 	lo = (struct layoutdriver_io_operations *)req->wb_ops;
 	if (lo->free_request_data)
 		lo->free_request_data(req);
+}
+
+void pnfs_free_fsdata(void *fsdata)
+{
+	struct pnfs_fsdata *data = (struct pnfs_fsdata *)fsdata;
+
+	if (data && data->ld_io_ops->free_fsdata)
+		data->ld_io_ops->free_fsdata(data);
 }
 
 /* Callback operations for layout drivers.
