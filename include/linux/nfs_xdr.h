a9b3e412e1bbabc4912dcc6d62efa85ccef75003
Fred Isaman <iisaman@citi.umich.edu>
2008-03-25 16:24:19 -0400

    pnfs: allow io error to propagate up.
    This allows the panlayout driver to propagate error codes up as before.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- include/linux/nfs_xdr.h
+++ include/linux/nfs_xdr.h
@@ -1049,6 +1049,7 @@ struct nfs_read_data {
 	const struct rpc_call_ops *call_ops;
 	struct rpc_clnt		*pnfs_client;	/* Holds pNFS device across async calls */
 	int			pnfsflags;
+	int			pnfs_error;
 	__u64			orig_offset;
 	struct nfs_client	*ds_nfs_client;
 #endif /* CONFIG_PNFS */
@@ -1082,6 +1083,7 @@ struct nfs_write_data {
 	struct rpc_clnt		*pnfs_client;	/* Holds pNFS device across
 						   async calls */
 	int			pnfsflags;
+	int			pnfs_error;
 	__u64			orig_offset;
 	int			how;		/* for FLUSH_STABLE */
 	struct nfs_client	*ds_nfs_client;
