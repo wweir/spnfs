df043be0b3419988fa9cd589b1ddbdb12ecab249
Fred <iisaman@citi.umich.edu>
2008-03-25 16:24:23 -0400

    pnfs: pnfs_write_begin
    Add hooks in the nfs_write_begin path, giving a driver the potential
    to read in page data around the data that is about to be copied to the page.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- include/linux/nfs4_pnfs.h
+++ include/linux/nfs4_pnfs.h
@@ -48,6 +48,12 @@ struct pnfs_layout_type {
 	u8 ld_data[];			/* layout driver private data */
 };
 
+struct pnfs_fsdata {
+	struct layoutdriver_io_operations *ld_io_ops;
+	u8 ld_fsdata[];
+};
+
+
 static inline struct inode *
 PNFS_INODE(struct pnfs_layout_type *lo)
 {
@@ -133,8 +139,11 @@ struct layoutdriver_io_operations {
 			       unsigned nr_pages, loff_t offset, size_t count,
 			       int sync, struct nfs_write_data *nfs_data);
 	int (*flush_one) (struct pnfs_layout_segment *, struct list_head *head, unsigned int npages, size_t count, int how);
+	int (*write_begin) (struct pnfs_layout_segment *lseg, struct page *page,
+			    loff_t pos, unsigned count,
+			    struct pnfs_fsdata **fsdata);
 	void (*free_request_data) (struct nfs_page *);
-
+	void (*free_fsdata) (struct pnfs_fsdata *data);
 
 	/* Consistency ops */
 	/* 2 problems:
@@ -172,6 +181,10 @@ struct layoutdriver_policy_operations {
 
 	/* test for nfs page cache coalescing */
 	int (*pg_test)(struct nfs_pageio_descriptor *, struct nfs_page *, struct nfs_page *);
+
+	/* Test for pre-write request flushing */
+	int (*do_flush)(struct pnfs_layout_segment *lseg, struct nfs_page *req,
+			struct pnfs_fsdata *fsdata);
 
 	/* Retreive the block size of the file system.  If gather_across_stripes == 1,
 	 * then the file system will gather requests into the block size.
