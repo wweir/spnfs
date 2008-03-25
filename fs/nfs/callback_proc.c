5c509d654c3007674a30684891291f9e35a47db7
Fred Isaman <iisaman@citi.umich.edu>
2008-03-25 16:24:16 -0400

    pnfs: pnfs_cb_layoutrecall proc implementation
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    pnfs: do not use args in pnfs_recall_layout after the invoking thread is woken up
    
    The invoker of the pnfs_recall_layout thread exits after
    wait_for_completion(&data.started), hence the kthread must use its args (that
    reside on the caller's stack) after calling complete(&args->started);
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    pnfs: fix inode ref bug in pnfs_recall_layout
    
    The kthread running pnfs_recall_layout did not
    put the inode it was recalling layouts from in the RECALL_FILE
    case.
    
    Symptom showed as busy inodes when umounting the file system
    after layouts were recalled.
    
    Feb 11 18:47:17 bh-testlin1 kernel: VFS: Busy inodes after unmount of 0:1b. Self-destruct in 5 seconds.  Have a nice day...
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    pnfs: fix compile problems if CONFIG_PNFS turned off - callback_proc.c
    
    Mostly, #include of header files should not encosed in #ifdef,
    since *.h files themselves have the appropriate #ifdefs, and they may want
    to define no-op versions of some functions.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/callback_proc.c
+++ fs/nfs/callback_proc.c
@@ -13,8 +13,12 @@
 #include "internal.h"
 
 #if defined(CONFIG_PNFS)
+#include <linux/kthread.h>
+#include <linux/module.h>
 #include <linux/writeback.h>
 #endif
+
+#include "pnfs.h"
 
 #ifdef NFS_DEBUG
 #define NFSDBG_FACILITY NFSDBG_CALLBACK
@@ -145,6 +149,161 @@ nfs_layoutrecall_find_inode(struct nfs_client *clp,
 
 	dprintk("%s: Return inode=%p\n", __func__, ino);
 	return ino;
+}
+
+struct recall_layout_threadargs {
+	struct inode *inode;
+	struct nfs_client *clp;
+	struct completion started;
+	struct cb_pnfs_layoutrecallargs rl;
+	int result;
+};
+
+static int pnfs_recall_layout(void *data)
+{
+	struct inode *inode, *ino;
+	struct nfs_client *clp;
+	struct nfs_server *server = NULL;
+	struct cb_pnfs_layoutrecallargs rl;
+	struct recall_layout_threadargs *args =
+		(struct recall_layout_threadargs *)data;
+	struct nfs4_pnfs_layoutreturn_arg lr_arg;
+	int status;
+
+	daemonize("nfsv4-layoutreturn");
+
+	dprintk("%s: recall_type=%d fsid 0x%llx-0x%llx start\n",
+		__func__, args->rl.cbl_recall_type,
+		args->rl.cbl_fsid.major, args->rl.cbl_fsid.minor);
+
+	clp = args->clp;
+	inode = args->inode;
+	server = NFS_SERVER(inode);
+	rl = args->rl;
+	args->result = 0;
+	complete(&args->started);
+	args = NULL;
+	/* Note: args must not be used after this point!!! */
+
+/* FIXME: need barrier here:
+   pause I/O to data servers
+   pause layoutgets
+   drain all outstanding writes to storage devices
+   wait for any outstanding layoutreturns and layoutgets mentioned in
+   cb_sequence.
+   then return layouts, resume after layoutreturns complete
+ */
+
+	if (rl.cbl_recall_type == RECALL_FILE) {
+		pnfs_return_layout(inode, &rl.cbl_seg);
+		goto out;
+	}
+
+	rl.cbl_seg.offset = 0;
+	rl.cbl_seg.length = NFS4_LENGTH_EOF;
+
+	/* FIXME: This loop is inefficient, running in O(|s_inodes|^2) */
+	while ((ino = nfs_layoutrecall_find_inode(clp, &rl)) != NULL) {
+		pnfs_return_layout(ino, &rl.cbl_seg);
+		iput(ino);
+	}
+
+	/* send final layoutreturn */
+	lr_arg.reclaim = 0;
+	lr_arg.layout_type = server->pnfs_curr_ld->id;
+	lr_arg.return_type = rl.cbl_recall_type;
+	lr_arg.lseg = rl.cbl_seg;
+	lr_arg.inode = inode;
+
+	status = pnfs_return_layout_rpc(server, &lr_arg);
+	if (status)
+		printk(KERN_INFO
+		       "%s: ignoring pnfs_return_layout_rpc status=%d\n",
+		       __func__, status);
+out:
+	iput(inode);
+	module_put_and_exit(0);
+	dprintk("%s: exit status %d\n", __func__, 0);
+	return 0;
+}
+
+/*
+ * Asynchronous layout recall!
+ */
+static int pnfs_async_return_layout(struct nfs_client *clp, struct inode *inode,
+				    struct cb_pnfs_layoutrecallargs *rl)
+{
+	struct recall_layout_threadargs data = {
+		.clp = clp,
+		.inode = inode,
+	};
+	struct task_struct *t;
+	int status;
+
+	/* should have returned NFS4ERR_NOMATCHING_LAYOUT... */
+	BUG_ON(inode == NULL);
+
+	data.rl = *rl;
+
+	init_completion(&data.started);
+	__module_get(THIS_MODULE);
+
+	t = kthread_run(pnfs_recall_layout, &data, "%s", "pnfs_recall_layout");
+	if (IS_ERR(t)) {
+		printk(KERN_INFO "NFS: Layout recall callback thread failed "
+			"for client (clientid %08x/%08x)\n",
+			(unsigned)(clp->cl_clientid >> 32),
+			(unsigned)(clp->cl_clientid));
+		status = PTR_ERR(t);
+		goto out_module_put;
+	}
+	wait_for_completion(&data.started);
+	return data.result;
+out_module_put:
+	module_put(THIS_MODULE);
+	return status;
+}
+
+unsigned pnfs_cb_layoutrecall(struct cb_pnfs_layoutrecallargs *args,
+			      void *dummy)
+{
+	struct nfs_client *clp;
+	struct inode *inode = NULL;
+	unsigned res;
+
+	res = htonl(NFS4ERR_INVAL);
+	clp = nfs_find_client(args->cbl_addr, 4);
+	if (clp == NULL) {
+		dprintk("%s: no client for addr %u.%u.%u.%u\n",
+			__func__, NIPQUAD(args->cbl_addr));
+		goto out;
+	}
+
+	res = htonl(NFS4ERR_NOMATCHING_LAYOUT);
+	inode = nfs_layoutrecall_find_inode(clp, args);
+	if (inode == NULL) {
+		dprintk("%s: no inode for RECALL_FILE\n", __func__);
+
+		goto out_putclient;
+	}
+
+	if (args->cbl_recall_type == RECALL_FILE &&
+	    !NFS_I(inode)->current_layout)
+		goto out_putinode;
+
+	res = 0;
+	/* Set up a helper thread to actually return the delegation */
+	if (!pnfs_async_return_layout(clp, inode, args))
+		goto out_putclient;
+
+	res = htonl(NFS4ERR_RESOURCE);
+out_putinode:
+	iput(inode);
+out_putclient:
+	nfs_put_client(clp);
+out:
+	dprintk("%s: exit with status = %d\n", __func__, ntohl(res));
+	return res;
 }
 
 #endif /* defined(CONFIG_PNFS) */
