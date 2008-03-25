37797f3e483ec36a03a9b8ee46c3ef35bfdae034
Fred Isaman <iisaman@citi.umich.edu>
2008-03-25 16:24:16 -0400

    pnfs: read path set ds_size
    Use the pNFS ds_rsize for read pageio.
    
    When using the nfs page cache for pNFS I/O, it is necessary to setupe pages
    using the rsize of the storage device instead of the rsize of the MDS.
    
    In 2.6.18.3, there are several read I/O paths through the client code and
    the rsize was used in each of these paths to setup the nfs page cache. Since
    several functions used the rsize, an rpc_ops->rsize function makes sense.
    
    In 2.6.24, there is only one read I/O path through the client code. The
    interface to the nfs page cache has been re-written. It is now shared between
    the read and write code paths, and moved entirely to fs/nfs/pagelist.c.
    
    As a result, there is now only one place to set the rsize.
    
    This patch removes the rpc_ops->rsize function pointer and
    sets the rsize to the pNFS ds_size when pNFS is being used.
    
    pNFS is not being used if layoutget fails, or the count is below the
    threshold.
    
    Signed-off-by: Andy Adamson<andros@umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    
    pnfs: fix compile problems if CONFIG_PNFS turned off - read.c
    
    Mostly, #include of header files should not encosed in #ifdef,
    since *.h files themselves have the appropriate #ifdefs, and they may want
    to define no-op versions of some functions.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/read.c
+++ fs/nfs/read.c
@@ -23,6 +23,8 @@
 
 #include <asm/system.h>
 #include <linux/module.h>
+#include <linux/pnfs_xdr.h>
+#include "pnfs.h"
 
 #include "nfs4_fs.h"
 #include "internal.h"
@@ -623,6 +625,9 @@ int nfs_readpages(struct file *filp, struct address_space *mapping,
 			return -EBADF;
 	} else
 		desc.ctx = get_nfs_open_context(nfs_file_open_context(filp));
+#ifdef CONFIG_PNFS
+	pnfs_set_ds_size(inode, desc.ctx, pages, filp->f_pos, &rsize);
+#endif /* CONFIG_PNFS */
 	if (rsize < PAGE_CACHE_SIZE)
 		nfs_pageio_init(&pgio, inode, nfs_pagein_multi, rsize, 0);
 	else
