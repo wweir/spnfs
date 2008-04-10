2722876cbc37f06cd7e4444148269842ececa2cd
Benny Halevy <bhalevy@panasas.com>
2008-04-10 17:54:05 +0300

    nfs41: sunrpc: provide functions to create and destroy a svc_xprt for backchannel use
    For nfs41 callbacks we need an svc_xprt to process requests coming up the
    backchannel socket as rpc_rqst's that are transformed into svc_rqst's that
    need a rq_xprt to be processed.
    
    The svc_{udp,tcp}_create methods are too heavy for this job as svc_create_socket
    creates an actual socket to listen on while for nfs41 we're "reusing" the
    fore channel's socket.
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- include/linux/sunrpc/svcsock.h
+++ include/linux/sunrpc/svcsock.h
@@ -46,6 +46,8 @@ int		svc_addsock(struct svc_serv *serv,
 			    int *proto);
 void		svc_init_xprt_sock(void);
 void		svc_cleanup_xprt_sock(void);
+struct svc_xprt *svc_sock_create(struct svc_serv *serv, int prot);
+void		svc_sock_destroy(struct svc_xprt *);
 
 /*
  * svc_makesock socket characteristics
