b72c1b74d184d0dd9acf29399844188a82465a69
Fred Isaman <iisaman@citi.umich.edu>
2008-05-01 09:02:17 -0400

    pnfs: pnfs_write_end
    Add hooks in the nfs_write_end path, giving a driver the potential for
    post-copy manipulation of the page.
    
    Signed-off-by: Fred Isaman <iisaman@citi.umich.edu>
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/pnfs.c
+++ fs/nfs/pnfs.c
@@ -1596,6 +1596,23 @@ int _pnfs_write_begin(struct inode *inode, struct page *page,
 	return status;
 }
 
+/* Return 0 on succes, negative on failure */
+/* CAREFUL - what happens if copied < len??? */
+int _pnfs_write_end(struct inode *inode, struct page *page,
+		    loff_t pos, unsigned len,
+		    unsigned copied, struct pnfs_fsdata *fsdata)
+{
+	struct nfs_server *nfss = NFS_SERVER(inode);
+	struct pnfs_layout_segment *lseg;
+	int status;
+
+	lseg = pnfs_find_get_lseg(inode, pos, len, IOMODE_RW);
+	status = nfss->pnfs_curr_ld->ld_io_ops->write_end(inode, page,
+						pos, len, copied, fsdata);
+	put_lseg(lseg);
+	return status;
+}
+
 /* Given an nfs request, determine if it should be flushed before proceeding.
  * It should default to returning False, returning True only if there is a
  * specific reason to flush.
