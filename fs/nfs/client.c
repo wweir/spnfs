da9cf9a3157607cdf88ff818c3252af86805ef0d
Benny Halevy <bhalevy@panasas.com>
2008-06-02 23:56:30 +0300

    pnfs: export nfs_put_client
    needed for file layout driver module.
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>

--- fs/nfs/client.c
+++ fs/nfs/client.c
@@ -216,6 +216,7 @@ void nfs_put_client(struct nfs_client *clp)
 		nfs_free_client(clp);
 	}
 }
+EXPORT_SYMBOL(nfs_put_client);
 
 static int nfs_sockaddr_match_ipaddr4(const struct sockaddr_in *sa1,
 				 const struct sockaddr_in *sa2)
